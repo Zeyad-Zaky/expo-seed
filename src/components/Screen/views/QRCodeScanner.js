7d683068509547d20f6acb3be5ae9817c1f25efc
ZiadFathi <zeyadyasserfathi@gmail.com>
2019-07-09 19:49:47 +0200

    Adding QRCode page

---
+++ src/components/Screen/views/QRCodeScanner.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import { View, Text, ImageBackground, Image, TouchableOpacity } from "react-native";
+// import styles from "../styles/QRCodeStyles";
+
+
+// const RANK_NAV_ICON = require("assets/QRCodeScannerPage/Rank_nav.png");
+// const EARTH_NAV_ICON = require("assets/QRCodeScannerPage/Earth_nav.png");
+// const FOOTBALLCOURT_NAV_ICON = require("assets/QRCodeScannerPage/FootballCourt_nav.png");
+// const MENU_LOGO = require("assets/QRCodeScannerPage/MENU.png");
+// const PLAYER_CARD_LOGO = require("assets/QRCodeScannerPage/PlayerCard.png");
+// const BACKGROUND = require("assets/QRCodeScannerPage/NEW BG.png");
+// const QRCODE = require("assets/QRCodeScannerPage/QR.png");
+
+class QRCodeScannerScreen extends Component {
+
+  // state = {
+  //   username: "Zeyad Yasser",
+  //   QRCode: QRCODE
+  // };
+  //
+  // render() {
+  //   return (
+  //     <View>
+  //       <ImageBackground
+  //         source={BACKGROUND}
+  //       >
+  //       </ImageBackground>
+  //     </View>
+  //   );
+  // }
+}
+
+export default QRCodeScannerScreen;
