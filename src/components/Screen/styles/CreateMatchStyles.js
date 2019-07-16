13bde1ea8f6641ff5f0ca3299df4409da6325892
abdelrahman977 <abdelrahman97@live.com>
2019-07-16 02:52:07 +0200

    Create match Day/pitch

--- src/components/Screen/styles/CreateMatchStyles.js
+++ src/components/Screen/styles/CreateMatchStyles.js
@@ -9,8 +9,8 @@ export default StyleSheet.create({
   background: {
     width: '100%',
     height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
   },
   menu: {
     marginLeft: '20%',
@@ -38,7 +38,111 @@ export default StyleSheet.create({
     right: '40%'
     
   },
+  pitchContainer: {
+    top: '15%',
+    left: '10%',
+    flex: 1,
+    alignItems: 'flex-start',
+    width: '90%',
+    height: '50%',
+  },
+  pitchContainerBG: {
+    resizeMode: 'stretch',
+  },
+  teamView: {
+    width: '100%',
+    height: '100%',
+    position: 'relative'
 
+  },
+  selectViewPitch: {
+    left: '1%',
+    flexDirection: 'row',
+
+  },
+  selectViewDay: {
+    left: '2%',
+    bottom: '1%',
+    flexDirection: 'row',
+
+  },
+  select: {
+    width: 110/1.2,
+    height: 62/1.2,
+    resizeMode: 'stretch',
+    marginEnd: '10%'
+  },
+  selectText: {
+    top: '5%',
+    color: 'white',
+    fontSize: 30,
+    textAlign: "center",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    marginEnd: '15%'
+  },
+  selectTextN: {
+    top: '5%',
+    color: 'white',
+    fontSize: 20,
+    textAlign: "center",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    marginEnd: '1%'
+  },
+  selectTextN1: {
+    top: '5%',
+    color: 'white',
+    fontSize: 20,
+    textAlign: "center",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    marginStart: '5%',
+    marginEnd: '10%'
+  },
+  teamInfo: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    marginBottom: '0.5%',
+    marginStart: '2%',
+  },
+  teamImage: {
+    width: 40,
+    height: 60,
+  },
+  teamImageStyle: {
+    width: '100%',
+    height: '100%',
+  },
+  spectator: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '1%',
+  },
+  spectatorImage: {
+    width: '85%',
+    height: '100%'
+  },
+  dayContainer: {
+    top: '15%',
+    left: '5%',
+    flex: 1,
+    alignItems: 'flex-start',
+    width: '95%',
+    height: '40%',
+  
+  },
+  dayContainerBG: {
+    resizeMode: 'stretch',
+  },
+  chooseDays: {
+    top: '20%',
+    left: '2%',
+    width: '95%',
+    height: '45%'
+
+  },
   navbar: {
     marginLeft: '15%',
     maxHeight: '10%',
